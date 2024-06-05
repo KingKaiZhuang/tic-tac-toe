dfea3b3b6da3c45168d76eef1c848cc1a207f5d1
KingKaiZhuang <5b0g0024@stust.edu.tw>
2024-06-05 11:46:27 +0800

    feat: Add click event handler to prevent playing on occupied squares

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,6 +4,10 @@ import "../css/tictactoe.css";
 
 export default function Board({ xIsNext, squares, onPlay }) {
   const handleClick = (i) => {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
     console.log("Square", i);
     if (xIsNext) {
       nextSquares[i] = "X";
@@ -12,6 +16,31 @@ export default function Board({ xIsNext, squares, onPlay }) {
     }
     onPlay(nextSquares);
   };
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length - 1; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+      return null;
+    }
+  };
+
   const nextSquares = squares.slice();
   let status = "下一個玩家：" + (xIsNext ? "X" : "O");
   return (
